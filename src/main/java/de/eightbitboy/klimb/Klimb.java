bf5a2dbd1563d164d7300ca27b755fc2c294bc58
EightBitBoy <haertel.dev@gmail.com>
2013-02-14 23:10:10 +0100

    advanced hello world

--- src/main/java/de/eightbitboy/klimb/Klimb.java
+++ src/main/java/de/eightbitboy/klimb/Klimb.java
@@ -1,8 +1,25 @@
 package de.eightbitboy.klimb;
 
-public class Klimb {
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+
+public class Klimb extends com.jme3.app.SimpleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Klimb app = new Klimb();
+		app.start();
+	}
+
+	@Override
+	public void simpleInitApp() {
+		Box b = new Box(1, 1, 1);
+		Geometry geom = new Geometry("Box", b);
+		Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+		mat.setColor("Color", ColorRGBA.Blue);
+		geom.setMaterial(mat);
+		rootNode.attachChild(geom);
 	}
 }
